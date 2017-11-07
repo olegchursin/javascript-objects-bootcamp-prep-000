02b1bf8fd6859a07e3d619ae5e62d86d704f27b1
Oleg Chursin <olegchursin@gmail.com>
2017-11-07 05:34:05 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  Metallica: "Enter Sandman",
+  ACDC: "Back in Black",
+  Queens: "Bohemian Rhapsody"
+};
+
+function updatePlaylist(playlist,artistName,songTitle) {
+  playlist['Guns and Roses'] = "Welcome to the Jungle";
+  return playlist
+};
