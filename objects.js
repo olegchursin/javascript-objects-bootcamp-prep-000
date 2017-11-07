061c1e981446e09f7f7f4d3404d56c899fa60dc2
Oleg Chursin <olegchursin@gmail.com>
2017-11-07 05:37:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ var playlist = {
   Queens: "Bohemian Rhapsody"
 };
 
-function updatePlaylist(playlist,artistName,songTitle) {
-  playlist.artistName = songTitle;
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist["artistName"] = songTitle;
   return playlist
 };
